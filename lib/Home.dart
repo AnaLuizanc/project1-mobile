ce120dbc81f21542a16e0e3d380f54c216f8fd6d
AnaLuizanc <analuizanc13@gmail.com>
2025-09-26 10:06:32 -0300

    refactor Home and main screens with new layout and navigation features

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,34 +8,78 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _contador = 0;
-
-  void _incrementVariable(){
-    setState(() {
-      ++_contador;
-    });
-  }
-
   @override
-  Widget build(BuildContext context, int contador) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Main Screen"),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.lime,
+        title: const Text('Home Page'),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Clique no botão para adicionar o contador"),
-            Text("$_contador"),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_mail),
+              title: Text('Contact Us'),
+            ),
           ],
         ),
       ),
+      body: const Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementVariable,
-        child: Icon(Icons.add),
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.lime,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.orangeAccent,
+        selectedItemColor: Colors.pink,
+        items:[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_call),
+              label: 'Add Call'
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Home.dart';
+import 'Home.dart';
 
 void main() {
-  runApp(Pedidos());
+  runApp(const MyApp());
 }
 
-class Pedidos extends StatelessWidget {
-   const Pedidos({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // retira a tarja de debug 
-      title: "Pedidos",
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.dark()
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home(), //cha a tela principal
+      home: const Home(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
